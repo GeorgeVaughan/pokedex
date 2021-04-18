dd9921b8ca5b2dacbae08a071ce8e48701026933
George Vaughan <georgio224@gmail.com>
2021-04-18 14:38:59 +0100

    Added search box to filter list of pokemon

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { PokemonOverviewModel } from "../models/PokemonOverviewModel";
 
@@ -6,10 +7,18 @@ type PokemonListProps = {
 };
 
 export const PokemonList = ({ pokemon }: PokemonListProps) => {
+  const [search, setSearch] = useState("");
+
+  const filteredPokemon = pokemon.filter((x) =>
+    x.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <div>
-      {pokemon.map(({ name }) => (
-        <div>
+      Search:{" "}
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      {filteredPokemon.map(({ name }) => (
+        <div key={name}>
           <Link to={`/pokemon/${name}`}>{name}</Link>
         </div>
       ))}
