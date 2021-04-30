3a375b383d2432cfe0fefaee3bc5fa0620ecee50
George Vaughan <georgio224@gmail.com>
2021-04-30 23:47:07 +0100

    Added a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { PokemonList } from "./components/PokemonList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { PokemonDetails } from "./components/PokemonDetails";
+import { Header, HeaderName } from "carbon-components-react";
 
 import "./App.css";
 
@@ -8,15 +9,24 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route
-            path="/pokemon/:name"
-            render={({ match }) => <PokemonDetails name={match.params.name} />}
-          />
-          <Route path="/">
-            <PokemonList />
-          </Route>
-        </Switch>
+        <Header aria-label="Pokedex">
+          <HeaderName href="#" prefix="Pokemon">
+            Pokedex
+          </HeaderName>
+        </Header>
+        <div style={{ marginTop: "30px" }}>
+          <Switch>
+            <Route
+              path="/pokemon/:name"
+              render={({ match }) => (
+                <PokemonDetails name={match.params.name} />
+              )}
+            />
+            <Route path="/">
+              <PokemonList />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
